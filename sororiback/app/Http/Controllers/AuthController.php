1a315803039788b776e8864187dc056e0d972e42
MarkLopezM <malomo@fp.insjoaquimmir.cat>
2024-05-13 17:35:55 +0200

    cambio menor en authcontroller

--- sororiback/app/Http/Controllers/AuthController.php
+++ sororiback/app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Town;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
